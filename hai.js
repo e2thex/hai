7eb6bd34ead0c8bcbe9aa023ec750671fc98e330
Erik Summerfield <esummerfield@phase2technology.com>
2012-09-04 12:54:46 -0500

    some updates that might make hes move to a new version

--- hai.js
+++ hai.js
@@ -1,3 +1,4 @@
+/****** JQUERY HELPER FUNCTION ******/
 $.fn.getHaiDatum = function() {
   return $(this).closest("trail").data('datum');
 };
@@ -16,11 +17,12 @@ $.fn.haiMoveAdd = function(how) {
   $(this).getHaiAdd().find('input').focus();
 };
 
+
+/******** main HAI CLASS ******/
 hai = function(db) {
   var that = {}
   that.db = db;
   that.resize = function() {
-
     $(this.html).find('.hai-trail-collection').each(function() {
       var maxWidth = 0;
       var trail_values = $('iHopeThisTagDoesNotExists');
@@ -40,7 +42,6 @@ hai = function(db) {
       //$(this).parent().css('min-height', h + 25);
     });
   };
-
   that.html = function() {
     hai = this;
     var hai_html = $(
@@ -50,52 +51,15 @@ hai = function(db) {
       "</hia-header>" +
       "<hai-data>" +
       "  <trail><subject class = 'hai-trail-collection'>" +
-      "    <trail id = 'add' class = 'add'><trail_value><input></input></trail_value></trail>" +
+      "    <trail id = 'add' class = 'add'><trail_value></trail_value></trail>" +
       "  </subject></trail>" +
       "</hai-data>" +
       "</hai>"
     );
-
-    hai_html.find(".add input").bind("save", function(e) {
-      var trail = $(this).closest("trail");
-      var is_subject = !($(trail).is("predicate > trail") ||$(trail).is("object > trail"))
-      var newtrail = false;
-      var type = false;
-      if (parse = /^(.*)##(.*)$/.exec($(this).val())) {
-          type = parse[2];
-        $(this).val(parse[1] == ''? '@' :parse[1]);
-      }
-      if($(this).val().match(/^@$/)) {
-        $(this).val(aspot.uuid());
-
-      }
-      if(datum = trail.data('currentDatum')) {
-        datum = datum.update($(this).val());
-        newtrail = datum.trail
-        trail.data('currentDatum', false);
-      }
-      else {
-        newdatum = is_subject ?
-          $(this).getHai().datum($(this).val()) :
-          trail.closest(".hai-trail-collection").closest("trail").data("datum").add($(this).val());
-      }
-      $(this).val('');
-      if(type) {
-        var pred = newdatum.add("type of");
-        var obj = pred.add(type);
-        pred.trail.children('.hai-trail-collection').first().append(obj.trail);
-        newdatum.trail.children('.hai-trail-collection').first().append(pred.trail);
-        var autofields = $(this).getHai().db.query('[VALUE = "'+type+'"]."hai-default-field"');
-        autofields.forEach(function (d) {
-          var pred = newdatum.add(d.value);
-          newdatum.trail.children('.hai-trail-collection').first().append(pred.trail);
-        });
-      }
-      trail.before(newdatum.trail);
-    });
+    hai_html.find(".add trail_value").append(haiInterface.basicTextField());
     // move down the 
-    hai_html.find(".add input").bind("moveToEndOfPrevious", function(e) {
-      var fs = $(this).closest("trail");
+    hai_html.find(".add").bind("moveToEndOfPrevious", function(e) {
+      var fs = $(this);
       var branch = $(fs);
       while(!branch.prev().length) {
         if(branch.closest(".hai-trail-collection").parent().length) {
@@ -108,10 +72,10 @@ hai = function(db) {
       branch.prev().find(".hai-trail-collection").last().haiMoveAdd('append');
       fs.closest("hai").data("hai").resize();
       fs.find("input").focus();
-    });
+    })
     // move down the trail
-    hai_html.find(".add input").bind("moveToParent", function(e) {
-      var fs = $(this).closest("trail");
+    .bind("moveToParent", function(e) {
+      var fs = $(this);
       if($(fs).closest(".hai-trail-collection").is("object")) {
         //can not move down trail from object need to remove pedicate first
         return;
@@ -119,17 +83,16 @@ hai = function(db) {
       $(fs).parent().closest("trail").closest(".hai-trail-collection:not(object)").append(fs);
       fs.closest("hai").data("hai").resize();
       fs.find("input").focus();
-    });
+    })
     //move up the trail
-    hai_html.find(".add input").bind("moveToLastChild", function(e) {
-      var fs = $(this).closest("trail");
+    .bind("moveToLastChild", function(e) {
+      var fs = $(this);
       $(fs).prev().children(".hai-trail-collection").children().last().children(".hai-trail-collection").append(fs);
       fs.closest("hai").data("hai").resize();
       fs.find("input").focus();
-    });
-
-    hai_html.find(".add input").bind("moveToEndOfNext", function(e) {
-      var fs = $(this).closest("trail");
+    })
+    .bind("moveToEndOfNext", function(e) {
+      var fs = $(this);
       var branch = $(fs);
       while(!branch.next().length) {
         if(branch.closest(".hai-trail-collection").parent().length) {
@@ -151,45 +114,9 @@ hai = function(db) {
       branch.append(fs);
       fs.closest("hai").data("hai").resize();
       fs.find("input").focus();
-    });
-    hai_html.find(".add input").bind("focusParent", function(e) {
-      $(this).closest('trail').parent().closest("trail").children("trail_value").children("value").focus();
-    });
-    hai_html.find(".add input").keyup(function(e) {
-      if($(this).val().match(/  $/)) {
-        $(this).val($(this).val().replace(/  $/,''));
-        $(this).trigger("save");
-        $(this).trigger("moveToEndOfPrevious");
-      }
-    });
-    hai_html.find(".add input").keydown(function(e) {
-      if($(this).val().match(/^$/)) {
-        if(e.which == 13) { //enter
-          $(this).trigger("moveToParent");
-        }
-        if(e.which == 37) { //left arrow
-          $(this).trigger("moveToParent");
-        }
-        if(e.which == 39) { //right arrow
-          $(this).trigger("moveToLastChild");
-        }
-        if(e.which == 38) { //up arrow
-          $(this).trigger("moveToEndOfPrevious");
-        }
-        if(e.which == 40) { //down arrow
-          $(this).trigger("moveToEndOfNext");
-        }
-        if(e.which == 8) { //backspase
-          $(this).trigger("focusParent");
-        }
-      }
-      console.log(e.which);
-    });
-    hai_html.find('#add input').autocomplete({
-      source:function(request, response) {
-        datum = $("#add").closest('.hai-trail-collection').closest('trail').data('datum');
-        response(datum.autocomplete(request.term));
-      }
+    })
+    .bind("focusParent", function(e) {
+      $(this).parent().closest("trail").children("trail_value").children("value").focus();
     });
     hai_html.find("#search").keydown(function(e) {
       if (e.which == 13) { //return
@@ -203,8 +130,13 @@ hai = function(db) {
         });
         }
         catch (e) {
+          var query = '[SELF = "' + $(this).val() +'"]';
+          if( $(this).val().match(/^##/)) {
+            query = '[."type of" = "'+$(this).val().replace(/^##/, '')+'"]';
+          }
+          console.log(query);
           try {
-            hai.db.query('[SELF = "' + $(this).val() +'"]').forEach(function(datum) {
+            hai.db.query(query).forEach(function(datum) {
               subjects.append(hai.datumBranch(datum).trail);
             });
           }
@@ -214,6 +146,8 @@ hai = function(db) {
         }
         subjects.append(add);
       }
+    })
+    .change(function(e) {
     });
     $(document).on("click", ".expand", function(e) {
       $(this).closest("trail").data("datum").expand();
@@ -325,3 +259,84 @@ hai = function(db) {
   return that;
 };
 
+haiInterface = {};
+haiInterface.basicTextField = function() {
+  var that = {};
+  var input = $('<input></input');
+
+    input.bind("save", function(e) {
+      var trail = $(this).closest("trail");
+      var is_subject = !($(trail).is("predicate > trail") ||$(trail).is("object > trail"))
+      var newtrail = false;
+      var type = false;
+      if (parse = /^(.*)##(.*)$/.exec($(this).val())) {
+          type = parse[2];
+        $(this).val(parse[1] == ''? '@' :parse[1]);
+      }
+      if($(this).val().match(/^@$/)) {
+        $(this).val(aspot.uuid());
+
+      }
+      if(datum = trail.data('currentDatum')) {
+        datum = datum.update($(this).val());
+        newtrail = datum.trail
+        trail.data('currentDatum', false);
+      }
+      else {
+        newdatum = is_subject ?
+          $(this).getHai().datum($(this).val()) :
+          trail.closest(".hai-trail-collection").closest("trail").data("datum").add($(this).val());
+      }
+      $(this).val('');
+      if(type) {
+        var pred = newdatum.add("type of");
+        var obj = pred.add(type);
+        pred.trail.children('.hai-trail-collection').first().append(obj.trail);
+        newdatum.trail.children('.hai-trail-collection').first().append(pred.trail);
+        var autofields = $(this).getHai().db.query('[VALUE = "'+type+'"]."hai-default-field"');
+        autofields.forEach(function (d) {
+          var pred = newdatum.add(d.value);
+          newdatum.trail.children('.hai-trail-collection').first().append(pred.trail);
+        });
+      }
+      trail.before(newdatum.trail);
+    })
+    .keyup(function(e) {
+      if($(this).val().match(/  $/)) {
+        $(this).val($(this).val().replace(/  $/,''));
+        $(this).trigger("save");
+        $(this).closest("trail").trigger("moveToEndOfPrevious");
+      }
+    })
+    .keydown(function(e) {
+      if($(this).val().match(/^$/)) {
+        var trail = $(this).closest("trail");
+        if(e.which == 13) { //enter
+          $(trail).trigger("moveToParent");
+        }
+        if(e.which == 37) { //left arrow
+          $(trail).trigger("moveToParent");
+        }
+        if(e.which == 39) { //right arrow
+          $(trail).trigger("moveToLastChild");
+        }
+        if(e.which == 38) { //up arrow
+          $(trail).trigger("moveToEndOfPrevious");
+        }
+        if(e.which == 40) { //down arrow
+          $(trail).trigger("moveToEndOfNext");
+        }
+        if(e.which == 8) { //backspase
+          $(trail).trigger("focusParent");
+        }
+      }
+      console.log(e.which);
+    })
+    .autocomplete({
+      source:function(request, response) {
+        datum = $("#add").closest('.hai-trail-collection').closest('trail').data('datum');
+        response(datum.autocomplete(request.term));
+      }
+    });
+  return input;
+}
